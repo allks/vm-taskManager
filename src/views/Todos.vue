41999d99ea865c439b2130ca705d6b8e1235629a
Aleks <a.s.gunchin@gmail.com>
2019-10-20 17:50:06 +0400

    add component Loader

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,28 +6,37 @@
       @add-todo="addTodo"
     />
     <hr>
-    <TodoList 
+    <Loader v-if="loading" />
+    <TodoList
+      v-else-if="todos.length"
       v-bind:todos="todos"
       @remove-todo="removeTodo"
     />
+    <p v-else>Задач нет</p>
   </div>
 </template>
 
 <script>
 import TodoList from '@/components/TodoList'
 import AddTodo from '@/components/AddTodo'
+import Loader from '@/components/Loader'
 export default {
   name: 'app',
   data(){
     return {
-      todos: []
+      todos: [],
+      loading: true
     }
   },
   mounted(){
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(response => response.json())
       .then(json => {
-        this.todos = json
+        setTimeout(() => {
+          this.todos = json
+          this.loading = false
+        }, 1000)
+        
       })
   },
   methods: {
@@ -39,7 +48,7 @@ export default {
     }
   },
   components: {
-    TodoList, AddTodo
+    TodoList, AddTodo, Loader
   }
 }
 </script>
